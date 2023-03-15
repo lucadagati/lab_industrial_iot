c24b4e232c5de9836e8b54dcfb24f7d2840ea823
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-03-15 23:35:52 +0100

    Update client.py

--- coap/ex2/client.py
+++ coap/ex2/client.py
@@ -1,9 +1,8 @@
-import asyncio
-from aiocoap import Context, Message, Code
-from coap_utils import print_coap_header  # Import the helper function
+import asyncio  # Import the asyncio module
+from aiocoap import Context, Message, Code  # Import the Context, Message, and Code classes from the aiocoap module
+from coap_utils import print_coap_header  # Import the print_coap_header helper function
 
-async def main():
-
+async def main():  # Define an asynchronous function called main
     # Create a CoAP context and set the client address
     context = await Context.create_client_context()
 
@@ -13,11 +12,11 @@ async def main():
     # Send the request and wait for the response
     response = await context.request(request).response
 
-    print_coap_header(response)  # Print the response header
+    print_coap_header(response)  # Print the response header using the helper function
 
     # Print the response
     print("Response code:", response.code)
     print("Payload:", response.payload.decode('utf-8'))
 
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ == "__main__":  # Check if the script is being run directly
+    asyncio.run(main())  # Run the main function using the asyncio event loop
