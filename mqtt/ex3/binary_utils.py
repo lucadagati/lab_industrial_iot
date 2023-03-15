3d0e725c96a90c0ba102e82a9d89e026fdaf929d
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-03-15 23:45:39 +0100

    Update binary_utils.py

--- mqtt/ex3/binary_utils.py
+++ mqtt/ex3/binary_utils.py
@@ -1,2 +1,5 @@
 def binary_representation(data):
-    return ' '.join(format(byte, '08b') for byte in data)
+    # Convert each byte in the input data to its binary representation and join them with spaces
+    binary_str = ' '.join(format(byte, '08b') for byte in data)
+    # Return the resulting string
+    return binary_str
