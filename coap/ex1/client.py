e5b1f7f83e72708560510b235222557f04f72270
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-03-15 23:34:51 +0100

    Update client.py

--- coap/ex1/client.py
+++ coap/ex1/client.py
@@ -1,21 +1,21 @@
-import asyncio
-from aiocoap import Context, Message
-from aiocoap.numbers.codes import Code
+import asyncio  # Import the asyncio module
+from aiocoap import Context, Message  # Import the Context and Message classes from the aiocoap module
+from aiocoap.numbers.codes import Code  # Import the Code class from the aiocoap.numbers.codes module
 
-async def main():
-    # Crea il contesto del client CoAP
+async def main():  # Define an asynchronous function called main
+    # Create a CoAP client context
     context = await Context.create_client_context()
 
-    while True:
-        # Leggi il messaggio dalla tastiera
+    while True:  # Loop forever
+        # Read a message from the keyboard
         messaggio = input("Inserisci un messaggio da inviare al server: ")
 
-        # Prepara la richiesta POST
+        # Prepare a POST request
         request = Message(code=Code.POST, payload=messaggio.encode('utf-8'))
         request.set_request_uri('coap://localhost/input')
 
-        # Invia la richiesta
+        # Send the request and wait for the response
         await context.request(request).response
 
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ == "__main__":  # Check if the script is being run directly
+    asyncio.run(main())  # Run the main function using the asyncio event loop
