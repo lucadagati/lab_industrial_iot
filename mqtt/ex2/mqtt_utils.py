e9a166e4c7dbec1ae7c81bf08c4708e86436e536
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-03-15 23:44:01 +0100

    Update mqtt_utils.py

--- mqtt/ex2/mqtt_utils.py
+++ mqtt/ex2/mqtt_utils.py
@@ -1,5 +1,10 @@
+# Define a function to print an MQTT message
 def print_mqtt_message(topic, payload, qos):
+    # Print a message header
     print("MQTT Message:")
+    # Print the topic of the message
     print(f"  Topic: {topic}")
+    # Print the quality of service (QoS) level of the message
     print(f"  QoS: {qos}")
+    # Print the payload of the message, decoded from bytes to a string
     print(f"  Payload: {payload.decode()}")
