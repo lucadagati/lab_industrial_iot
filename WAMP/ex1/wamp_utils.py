29ea83320e2dbe5e339a0c81fcdb26ad53154aab
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-03-15 23:27:54 +0100

    Update wamp_utils.py

--- WAMP/ex1/wamp_utils.py
+++ WAMP/ex1/wamp_utils.py
@@ -1,4 +1,4 @@
-import random
+import random  # Import the random module for generating random numbers
 
-def random_temperature():
-    return round(random.uniform(20, 30), 2)
+def random_temperature():  # Define a function called random_temperature
+    return round(random.uniform(20, 30), 2)  # Generate a random floating-point number between 20 and 30 (inclusive) using the uniform method of the random module, round it to 2 decimal places using the round function, and return the result
