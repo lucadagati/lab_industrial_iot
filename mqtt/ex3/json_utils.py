3c4a9276c0fca1e67d7dd0f77d5a1b42d5a290c7
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-03-15 23:46:08 +0100

    Update json_utils.py

--- mqtt/ex3/json_utils.py
+++ mqtt/ex3/json_utils.py
@@ -2,11 +2,18 @@ import json
 import time
 
 def encode_json_message(temperature):
+    # Create a dictionary representing the message
     message = {
         'temperature': temperature,
-        'timestamp': time.time(),
+        'timestamp': time.time(),  # Add the current timestamp to the message
     }
-    return json.dumps(message).encode('utf-8')
+    # Convert the dictionary to a JSON-encoded string and encode it as bytes using UTF-8
+    encoded_message = json.dumps(message).encode('utf-8')
+    # Return the resulting bytes object
+    return encoded_message
 
 def decode_json_message(encoded_message):
-    return json.loads(encoded_message.decode('utf-8'))
+    # Decode the bytes object as a UTF-8 string and parse it as a JSON object
+    decoded_message = json.loads(encoded_message.decode('utf-8'))
+    # Return the resulting dictionary
+    return decoded_message
