106e826c7c2e93fea20e5c105da50f8d679420cb
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-03-15 23:38:53 +0100

    Update client_temp.py

--- coap/ex5/client_temp.py
+++ coap/ex5/client_temp.py
@@ -2,17 +2,17 @@ import asyncio
 from aiocoap import Context, Message, Code
 
 async def main():
-    context = await Context.create_client_context()
+    context = await Context.create_client_context()  # Create a CoAP client context
 
-    request = Message(code=Code.GET, uri="coap://127.0.0.1:5683/temperature")
-    response = await context.request(request).response
-    print("Initial temperature:", response.payload.decode('utf-8'))
+    request = Message(code=Code.GET, uri="coap://127.0.0.1:5683/temperature")  # Create a CoAP GET request to retrieve the temperature resource
+    response = await context.request(request).response  # Send the request and wait for the response
+    print("Initial temperature:", response.payload.decode('utf-8'))  # Print the initial temperature
 
-    request.opt.observe = 0  # Add the "Observe" option
-    observation = context.request(request)
+    request.opt.observe = 0  # Add the "Observe" option to the request
+    observation = context.request(request)  # Send the observation request to the server
 
-    async for response in observation.observation:
-        print("Updated temperature:", response.payload.decode('utf-8'))
+    async for response in observation.observation:  # Start listening for updates
+        print("Updated temperature:", response.payload.decode('utf-8'))  # Print the updated temperature
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main())  # Run the main coroutine
