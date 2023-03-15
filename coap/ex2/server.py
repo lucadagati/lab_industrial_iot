9aeb368c29a532bfbcf34f2350ea6734976a368b
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-03-15 23:36:20 +0100

    Update server.py

--- coap/ex2/server.py
+++ coap/ex2/server.py
@@ -1,16 +1,17 @@
-import asyncio
-from aiocoap import resource, Context, Message, Code
-from coap_utils import print_coap_header  # Import the helper function
+import asyncio  # Import the asyncio module
+from aiocoap import resource, Context, Message, Code  # Import the Context, Message, and Code classes from the aiocoap module
+from coap_utils import print_coap_header  # Import the print_coap_header helper function
 
 class HelloWorldResource(resource.Resource):
     async def render_get(self, request):
-        print_coap_header(request)  # Print the request header
+        print_coap_header(request)  # Print the request header using the helper function
 
+        # Create a CoAP response
         response = Message(code=Code.CONTENT)
         response.payload = b"Hello, CoAP!"
         return response
 
-async def main():
+async def main():  # Define an asynchronous function called main
     # Create a CoAP server with the HelloWorldResource
     root = resource.Site()
     root.add_resource(('hello',), HelloWorldResource())
@@ -20,5 +21,5 @@ async def main():
     print("CoAP server started on 127.0.0.1:5683")
     await asyncio.sleep(3600)  # Keep the server running for 1 hour
 
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ == "__main__":  # Check if the script is being run directly
+    asyncio.run(main())  # Run the main function using the asyncio event loop
