b44ad586d4f9164dff017425aae392cd06a78aad
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-03-15 23:37:30 +0100

    Update coap_utils.py

--- coap/ex2/coap_utils.py
+++ coap/ex2/coap_utils.py
@@ -1,11 +1,16 @@
-def print_coap_header(message):
-    print("CoAP Header:")
-    print(f"  Version: {message.version}")
-    print(f"  Type: {message.mtype}")  # Change 'type' to 'mtype'
-    print(f"  Token Length: {len(message.token)}")
-    print(f"  Code: {message.code}")
-    print(f"  Message ID: {message.mid}")
+# This is a function called `print_coap_header` that takes a `message` object as input and prints out the CoAP header and options of the message.
+# This function is useful for debugging CoAP messages, as it can provide more detailed information about the message header and options.
 
+def print_coap_header(message):
+    # Print the CoAP header
+    print("CoAP Header:")
+    print(f"  Version: {message.version}")  # Print the CoAP version
+    print(f"  Type: {message.mtype}")  # Print the CoAP message type
+    print(f"  Token Length: {len(message.token)}")  # Print the length of the CoAP token
+    print(f"  Code: {message.code}")  # Print the CoAP message code
+    print(f"  Message ID: {message.mid}")  # Print the CoAP message ID
+
+    # Print the message options
     print("Options:")
     for option_number, option_value in message.opt.option_list():
-        print(f"  {option_number}: {option_value}")
+        print(f"  {option_number}: {option_value}")  # Print each CoAP option number and value pair
