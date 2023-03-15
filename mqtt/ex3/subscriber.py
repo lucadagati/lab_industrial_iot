c21b9896508b52a7aa1b2f58f95a376b60157aeb
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-03-15 23:47:39 +0100

    Update subscriber.py

--- mqtt/ex3/subscriber.py
+++ mqtt/ex3/subscriber.py
@@ -1,36 +1,50 @@
 import time
-from paho.mqtt import client as mqtt_client
-from mqtt_utils import print_mqtt_message
-from json_utils import decode_json_message
-from binary_utils import binary_representation
+from paho.mqtt import client as mqtt_client  # Import the MQTT client library
+from mqtt_utils import print_mqtt_message  # Import the helper function for printing MQTT messages
+from json_utils import decode_json_message  # Import the function for decoding JSON messages
+from binary_utils import binary_representation  # Import the function for getting the binary representation of bytes
 
+broker = "mqtt.eclipseprojects.io"  # Set the MQTT broker address
+port = 1883  # Set the MQTT broker port number
+topic = "demo/temperature"  # Set the topic to subscribe to
 
-broker = "mqtt.eclipseprojects.io"
-port = 1883
-topic = "demo/temperature"
 
 def on_connect(client, userdata, flags, rc):
+    # This function is called when the client connects to the broker
     if rc == 0:
         print("Connected to MQTT broker!")
+        # Subscribe to the topic
         client.subscribe(topic)
     else:
         print(f"Failed to connect, return code {rc}")
 
+
 def on_message(client, userdata, msg):
+    # This function is called when a new message is received
     print("Received:")
-    print_mqtt_message(msg.topic, msg.payload, msg.qos)  # Print the message structure
+    # Print the message structure for debugging purposes
+    print_mqtt_message(msg.topic, msg.payload, msg.qos)
+    # Print the JSON message for debugging purposes
     print("JSON message:")
-    print(decode_json_message(msg.payload))  # Print the JSON message
+    print(decode_json_message(msg.payload))
+    # Print the binary representation of the message for debugging purposes
     print("Binary representation:")
-    print(binary_representation(msg.payload))  # Print the binary representation
+    print(binary_representation(msg.payload))
+
 
 def main():
+    # Create a new MQTT client instance
     client = mqtt_client.Client("subscriber")
+    # Set the callback functions for when the client connects to the broker and when a new message is received
     client.on_connect = on_connect
     client.on_message = on_message
+    # Connect the client to the broker
     client.connect(broker, port)
 
+    # Start the MQTT client loop, which will continue running forever
     client.loop_forever()
 
+
 if __name__ == "__main__":
+    # Call the main function if this file is run as the main program
     main()
